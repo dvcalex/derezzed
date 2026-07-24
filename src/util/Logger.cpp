df661461469eb9676594cd757d6449b26ea90ba8
alexd <jacrisspi77@gmail.com>
2026-07-23 20:56:37 -0500

    contiguous log buffer

--- src/util/Logger.cpp
+++ src/util/Logger.cpp
@@ -1,27 +1,28 @@
 #include "drz/util/Logger.hpp"
+
 #include <SDL3/SDL_log.h>
-#include <queue>
+#include <vector>
 
 namespace drz::logger
 {
 namespace
 { // anonymous for linkage only in this translation unit
-std::queue<std::string> log_queue;
+std::vector<std::string> log_buffer;
 }
 
 void log(const std::string& message)
 {
-    log_queue.push(message);
+    log_buffer.push_back(message);
 }
 
 void flush_log()
 {
-    while (!log_queue.empty())
+    while (!log_buffer.empty())
     {
         // TODO: Log into another stream if we want to log into a file or something, but for now just log to SDL's
         // logging system
-        SDL_Log("%s", log_queue.front().c_str());
-        log_queue.pop();
+        SDL_Log("%s", log_buffer.front().c_str());
+        log_buffer.clear();
     }
 }
 } // namespace drz::logger
